997d488426c0c48bca6b8037d5b02019492d0de2
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-03-11 20:44:26 +0100

    Word creation in app

---
+++ LanguageLearningApp/FormMain.cs
@@ -0,0 +1,48 @@
+﻿using LanguageLearning;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LanguageLearningApp
+{
+    public partial class FormMain : Form
+    {
+        WordManager wordManager = new();
+        public FormMain()
+        {
+            InitializeComponent();
+            wordManager = new();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvWords.DataSource = wordManager.GetAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAddWord_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Word word = new(tbWord.Text);
+                wordManager.Create(word);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
